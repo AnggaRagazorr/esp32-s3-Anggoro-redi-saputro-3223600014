d20ea4ad71e767ee22481ff2ed222b26a95afcd1
AnggaRagazorr <anggororedi@gmail.com>
2025-11-10 13:17:03 +0700

    Update button.ino

--- button/button.ino
+++ button/button.ino
@@ -3,18 +3,28 @@
 
 void taskButton(void *pvParameters) {
   while (1) {
-    int state = digitalRead(BTN);
-    digitalWrite(LED, !state);
-    delay(50);
+    if (digitalRead(BTN) == LOW) {
+      digitalWrite(LED, HIGH);
+      Serial.print("Button Pressed - Core: ");
+    } else {
+      digitalWrite(LED, LOW);
+      Serial.print("Button Released - Core: ");
+    }
+    Serial.println(xPortGetCoreID());
+    delay(200);
   }
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(BTN, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  //core 0
-  xTaskCreatePinnedToCore(taskButton, "Button_Task", 2000, NULL, 1, NULL, 0);
-  //core 1
-  //xTaskCreatePinnedToCore(taskButton, "Button_Task", 2000, NULL, 1, NULL, 1);
+
+  // Core 0
+  xTaskCreatePinnedToCore(taskButton, "taskButton_Core0", 2000, NULL, 1, NULL, 0);
+
+  // Core 1
+  // xTaskCreatePinnedToCore(taskButton, "taskButton_Core1", 2000, NULL, 1, NULL, 1);
 }
+
 void loop() {}
