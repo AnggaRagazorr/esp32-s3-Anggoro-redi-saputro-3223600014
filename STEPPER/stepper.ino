b1153004f9cf4c57ac94a836cf7e8c4e1457028c
AnggaRagazorr <anggororedi@gmail.com>
2025-11-10 13:23:54 +0700

    Update stepper.ino

--- STEPPER/stepper.ino
+++ STEPPER/stepper.ino
@@ -1,18 +1,15 @@
-// ====== PIN SESUAI DENGAN WIRING KAMU ======
 #define IN1 37  // B-
 #define IN2 38  // B+
 #define IN3 39  // A+
 #define IN4 40  // A-
 
-// Urutan langkah untuk full-step
 const int seq[4][4] = {
-  {1,0,1,0},  // Langkah 1
-  {0,1,1,0},  // Langkah 2
-  {0,1,0,1},  // Langkah 3
-  {1,0,0,1}   // Langkah 4
+  {1,0,1,0},
+  {0,1,1,0},
+  {0,1,0,1},
+  {1,0,0,1}
 };
 
-// Fungsi untuk gerak maju
 void stepForward(int steps) {
   for (int s = 0; s < steps; s++) {
     for (int i = 0; i < 4; i++) {
@@ -27,24 +24,25 @@ void stepForward(int steps) {
 
 void taskStepper(void *pvParameters) {
   while (1) {
-    stepForward(200);   // 1 putaran kira-kira
+    Serial.print("Stepper Task running on Core: ");
+    Serial.println(xPortGetCoreID());
+    stepForward(200);
     delay(500);
   }
 }
 
 void setup() {
-  // Inisialisasi pin
+  Serial.begin(115200);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  // core 0
+  // Core 0
   xTaskCreatePinnedToCore(taskStepper, "taskStepper_Core0", 4000, NULL, 1, NULL, 0);
 
-  // core 1 
+  // Core 1
   // xTaskCreatePinnedToCore(taskStepper, "taskStepper_Core1", 4000, NULL, 1, NULL, 1);
 }
 
 void loop() {}
-
